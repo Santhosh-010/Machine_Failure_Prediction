56d084dda46e1f83415868b18efc43b42afba405
Santhosh-010 <69196696+Santhosh-010@users.noreply.github.com>
2026-01-12 15:11:02 +0530

    Update data_register.py
    missed the cases of username and now updated it correctly as Santhu976.

--- week_2_mls/model_building/data_register.py
+++ week_2_mls/model_building/data_register.py
@@ -3,7 +3,7 @@ from huggingface_hub import HfApi, create_repo
 import os
 
 
-repo_id = "santhu976/machine-failure-prediction"
+repo_id = "Santhu976/machine-failure-prediction"
 repo_type = "dataset"
 
 # Initialize API client
